74028046aad5fdf4fbb3c9d87cf45b7436d8c749
nothing <nothing@example.com>
2026-07-16 18:00:51 +0800

    fix: unify application typography scale

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 import "./reference-ui.css";
+import "./typography.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
